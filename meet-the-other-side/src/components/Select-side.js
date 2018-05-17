1416958af9d09c244a09893804909d3ad4612bf0
SophieMdl <sophie.michaudel@gmail.com>
2018-05-17 17:24:30 +0200

    creation-home

--- meet-the-other-side/src/components/Select-side.js
+++ meet-the-other-side/src/components/Select-side.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import bgLight from '../img/light-side.jpg'
+import bgDark from '../img/dark-side.jpg'
+import './../style/select-side.css'
 
-const Button = ({ action, text }) =>
-    <button onClick={action}> {text} </button>
+const SelectSide = ({ action, text }) => {
+    return(
+        <div className="select-side-wrapper flex">
+            <h1 className="home-title">Choose your side</h1>
+            <div className="image-wrapper light-side">
+                <img className="image-choice" src={bgLight} alt={bgLight} />
+            </div>
+            <div className="image-wrapper dark-side">
+                <img className="image-choice" src={bgDark} alt={bgDark} />
+            </div>
+        </div>
+    )
+}
+    {/* <button onClick={action}> {text} </button> */}
 
-Button.Red = ({ action, text }) =>
-    <button onClick={action} style={{ color: 'red' }}> {text} </button>
 
-export default Button
+export default SelectSide
