088f713d4e51aedb7960e38697ccf1b75c8b138b
SophieMdl <sophie.michaudel@gmail.com>
2018-05-17 11:07:23 +0200

    fichiers initiaux

---
+++ meet-the-other-side/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './containers/app.js'
+
+const container = document.getElementById('container')
+
+ReactDOM.render(<App />, container)
