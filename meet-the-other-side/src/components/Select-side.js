f4a6930f0ec031efd1ce3519af90bf32cfdf17d3
kyerz <mikael.versu@gmail.com>
2018-05-18 02:39:30 +0200

    add light/dark effects on hover

--- meet-the-other-side/src/components/Select-side.js
+++ meet-the-other-side/src/components/Select-side.js
@@ -8,9 +8,11 @@ const SelectSide = ({ action, text }) => {
         <div className="select-side-wrapper flex">
             <h1 className="home-title">Choose your side</h1>
             <div className="image-wrapper light-side" onClick={action}>
+                <div className="light-side-children">Light</div>
                 <img className="image-choice" src={bgLight} alt={bgLight} />
             </div>
             <div className="image-wrapper dark-side" onClick={action}>
+            <div className="dark-side-children">Dark</div>
                 <img className="image-choice" src={bgDark} alt={bgDark} />
             </div>
         </div>
---
+++ meet-the-other-side/src/img/localhost_files/0.90790b90f643f5f375d3.hot-update.js
@@ -0,0 +1,24 @@
+webpackHotUpdate(0,{
+
+/***/ "./node_modules/css-loader/index.js?{\"importLoaders\":1}!./node_modules/postcss-loader/lib/index.js?{\"ident\":\"postcss\",\"plugins\":[null,null]}!./src/style/select-side.css":
+/*!*************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader?{"importLoaders":1}!./node_modules/postcss-loader/lib?{"ident":"postcss","plugins":[null,null]}!./src/style/select-side.css ***!
+  \*************************************************************************************************************************************************************/
+/*! dynamic exports provided */
+/*! all exports used */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(undefined);
+// imports
+
+
+// module
+exports.push([module.i, ".select-side-wrapper {\n    height: 100vh;\n    background-color: #000;\n}\n\n.flex {\n    display: -ms-flexbox;\n    display: flex;\n}\n\n.flex .image-wrapper {\n    width: 50%;\n}\n\n.image-choice {\n    -o-object-fit: cover;\n       object-fit: cover;\n    -o-object-position : top;\n       object-position : top;\n    width: 100%;\n    height: 100%;\n    opacity: 0.3;\n    position: relative;\n    cursor : pointer;\n}\n\n.light-side{\n    background : -webkit-gradient(linear, left bottom, left top, from(rgba(0, 80, 161, 0.2)), to(#000000));\n    background : -webkit-linear-gradient(bottom, rgba(0, 80, 161, 0.2), #000000);\n    background : -o-linear-gradient(bottom, rgba(0, 80, 161, 0.2), #000000);\n    background : linear-gradient(to top, rgba(0, 80, 161, 0.2), #000000);\n}\n.light-side-children, .dark-side-children{\n    font-family: 'starjedi';\n    font-size: 1.5rem;\n    color: #000;\n}\n.light-side-children{\n    margin: 20px 0 0 20px;\n}\n.dark-side-children{\n    float: right;\n    margin: 20px 20px 0 0;  \n}\n.image-choice:hover{\n    color:#fcc200;\n}\n.dark-side{\n    background : -webkit-gradient(linear, left bottom, left top, from(rgba(169, 34, 28, 0.2)), to(#000000));\n    background : -webkit-linear-gradient(bottom, rgba(169, 34, 28, 0.2), #000000);\n    background : -o-linear-gradient(bottom, rgba(169, 34, 28, 0.2), #000000);\n    background : linear-gradient(to top, rgba(169, 34, 28, 0.2), #000000);\n}\n.light-side:hover{\n    background : -webkit-gradient(linear, left bottom, left top, from(rgba(0, 80, 161, 1)), to(#000000));\n    background : -webkit-linear-gradient(bottom, rgba(0, 80, 161, 1), #000000);\n    background : -o-linear-gradient(bottom, rgba(0, 80, 161, 1), #000000);\n    background : linear-gradient(to top, rgba(0, 80, 161, 1), #000000);\n}\n.dark-side:hover{\n    background : -webkit-gradient(linear, left bottom, left top, from(rgba(169, 34, 28, 1)), to(#000000));\n    background : -webkit-linear-gradient(bottom, rgba(169, 34, 28, 1), #000000);\n    background : -o-linear-gradient(bottom, rgba(169, 34, 28, 1), #000000);\n    background : linear-gradient(to top, rgba(169, 34, 28, 1), #000000);\n}\n.charList {\n    display: none;\n}\n\n.home-title {\n    font-size: 45px;\n    position: absolute;\n    top: 45%;\n    text-align: center;\n    width: 100%;\n    z-index: 1;\n    color: #fcc200;\n    font-family: 'starjedi';\n}\n", ""]);
+
+// exports
+
+
+/***/ })
+
+})
+//# sourceMappingURL=0.90790b90f643f5f375d3.hot-update.js.map
